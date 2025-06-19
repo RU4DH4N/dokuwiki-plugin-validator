dc82822bcd0b42ba7ca0c042a25628f391070020
Ruadhan <76910193+RU4DH4N@users.noreply.github.com>
2025-06-19 14:47:17 +0100

    Update metadata.php

--- conf/metadata.php
+++ conf/metadata.php
@@ -1,4 +1,12 @@
 <?php
-
-$meta['sitekey'] = array('string');
-$meta['secretkey'] = array('string');
+$meta['cf-sitekey'] = array('string');
+$meta['cf-secretkey'] = array('string');
+$meta['g-sitekey'] = array('string');
+$meta['g-secretkey'] = array('string');
+$meta['mode'] = array(
+    'multichoice',
+    '_choices' => array(
+        'cloudflare',
+        'google',
+    ),
+);
